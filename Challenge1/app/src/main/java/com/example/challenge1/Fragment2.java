bdafc89a7457139c8a6be7fba946779f95b760f1
Sequeira12 <brunomsequeira@hotmail.com>
2024-10-10 18:08:43 +0100

    clean code

--- Challenge1/app/src/main/java/com/example/challenge1/Fragment2.java
+++ Challenge1/app/src/main/java/com/example/challenge1/Fragment2.java
@@ -14,66 +14,24 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Fragment2#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class Fragment2 extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+
 
     private ModelView viewModel;
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-    private Button button;
-    Animal animal;
 
-    private TextView animalAge;
-    private TextView animalName;
+    private Button button;
+
     public Fragment2() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Fragment2.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Fragment2 newInstance(String param1, String param2) {
-        Fragment2 fragment = new Fragment2();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_2, container, false);
-
-
-
-
         return view;
 
     }
@@ -97,10 +55,7 @@ public class Fragment2 extends Fragment {
                 String age = editTextAnimalAge.getText().toString();
                 checkChanges(name,age,animal);
 
-
-
                 Log.v("TAG","button clicked - Save");
-
                 ((MainActivity) getActivity()).switchToFragment1();
             }
         });
