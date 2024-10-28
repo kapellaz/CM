9a89aef8d7793008f4a7ad0f2b379c89dfd659d4
Sequeira12 <brunomsequeira@hotmail.com>
2024-10-28 23:36:27 +0000

    login/register, firebase DONE, app bar DONE

--- Challenge2/app/src/main/java/com/example/notes_app/NoteEdit.java
+++ Challenge2/app/src/main/java/com/example/notes_app/NoteEdit.java
@@ -8,11 +8,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Note#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class Note extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
@@ -28,23 +23,6 @@ public class Note extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Note.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Note newInstance(String param1, String param2) {
-        Note fragment = new Note();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
